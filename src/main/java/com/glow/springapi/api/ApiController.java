dbd4b6e18ac39a9eccc32614d870a3106d82274d
Glowstudent777 <cameron@themenkens.com>
2023-07-05 15:52:03 -0500

    Initial Commit

---
+++ src/main/java/com/glow/springapi/api/ApiController.java
@@ -0,0 +1,16 @@
+package com.glow.springapi.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @GetMapping
+    public HttpStatus Status() {
+        return HttpStatus.OK;
+    }
+}
